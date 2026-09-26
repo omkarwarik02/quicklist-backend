9c8d3f6dfe6b9f6ddc51000ad659ab87f1b930cc
OMKAR AVINASH WARIK <omkarwarik1204@gmail.com>
2026-09-26 22:24:08 +0530

    Created getListing route and delete route

--- src/controllers/listingController.ts
+++ src/controllers/listingController.ts
@@ -35,3 +35,35 @@ export const createListing = async (req:AuthRequest, res:Response) =>{
     res.status(500).json({ error: "Failed to create listing" });
  }
 }
+
+export const getMyListings = async (req:AuthRequest, res:Response) => {
+    try {
+        const seller = await User.findOne({firebaseUid:req.firebaseUser!.uid})
+       if(!seller) {
+            return res.status(404).json({error:"Seller not found"});
+        }
+        const listings = await Listing.find({ seller: seller._id}).sort({ createdAt: -1});
+        res.status(200).json({ listings});
+    } catch(error){
+        console.error("Get my listings error:", error);
+    res.status(500).json({ error: "Failed to fetch listings" });
+    }
+}
+
+export const deleteListing = async (req: AuthRequest, res: Response) => {
+    try{
+        const seller = await User.findOne({ firebaseUid: req.firebaseUser!.uid});
+        if(!seller) return res.status(404).json({ error:"Seller not found"});
+
+        const listing = await Listing.findById(req.params.id);
+        if(!listing) return res.status(404).json({ error: "Listing not found"});
+
+        if(listing.seller.toString() !== seller._id.toString()){
+            return res.status(403).json({ error: "Not authorized to delete this listing" });
+        }
+
+    } catch(error){
+         console.error("Delete listing error:", error);
+    res.status(500).json({ error: "Failed to delete listing" });
+    }
+}
--- src/routes/listingRoutes.ts
+++ src/routes/listingRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createListing } from "../controllers/listingController";
+import { createListing, getMyListings, deleteListing } from "../controllers/listingController";
+
 import { verifyFirebaseToken } from "../middleware/authMiddleware";
 
 const router = Router();
 
 router.post("/", verifyFirebaseToken, createListing);
+router.get("/self-listing", verifyFirebaseToken, getMyListings);
+router.delete("/:id", verifyFirebaseToken, deleteListing);
+
 
 export default router;
